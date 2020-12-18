c53a9f2a3fd9bdf4223de9cc5c702e1f8ad6c197
Zedd <gusdnre@gmail.com>
2020-12-18 16:06:51 +0900

    iOS 1강 개요 및 계산기 만들기

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	
+	@IBOutlet weak var display: UILabel!
+	
+	var userIsIntheMiddleOfTyping : Bool = false
+	
+	@IBAction func touchDigit(_ sender: UIButton) {
+		let digit = sender.currentTitle!
+		
+		if userIsIntheMiddleOfTyping {
+			let textCurrentlyInDisplay = display.text!
+			display.text = textCurrentlyInDisplay + digit
+		} else {
+			display.text = digit
+		}
+		
+		userIsIntheMiddleOfTyping = true
 	}
-
-
+	
+	@IBAction func performOperation(_ sender: UIButton) {
+		userIsIntheMiddleOfTyping = false
+		
+		if let mathematicalSymbol = sender.currentTitle {
+			if mathematicalSymbol == "π"{
+				display.text = String(Double.pi)
+			}
+		}
+	}
 }
 
